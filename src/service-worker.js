e5519fa56d67033bad5ba801aa1cedf5238fc39d
Rapahel Thiney <rthiney@mynexuscare.com>
2017-10-15 13:59:37 -0400

    updated js

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,24 +2,21 @@
  * Check out https://googlechrome.github.io/sw-toolbox/docs/master/index.html for
  * more info on how to use sw-toolbox to custom configure your service worker.
  */
-
-
 'use strict';
+
 importScripts('./build/sw-toolbox.js');
 
-self.toolbox.options.cache = {
-    name: 'ionic-cache'
-};
+self.toolbox.options.cache = { name: 'ionic-cache' };
 
 // pre-cache our key assets
 self.toolbox.precache(
-    [
-        './build/main.js',
-        './build/main.css',
-        './build/polyfills.js',
-        'index.html',
-        'manifest.json'
-    ]
+  [
+    './build/main.js',
+    './build/main.css',
+    './build/polyfills.js',
+    'index.html',
+    'manifest.json'
+  ]
 );
 
 // dynamically cache any other local assets
